b5c60f283d99d5b190fad7726c9ae2b910b867b7
Mester Richard <mrichard@citromail.hu>
2022-10-08 19:57:28 +0200

    HomeFragment:: Egy kép kiválasztása a galériából.

--- app/src/main/java/com/mrichard/napkin_handler/ui/home/HomeFragment.java
+++ app/src/main/java/com/mrichard/napkin_handler/ui/home/HomeFragment.java
@@ -1,30 +1,54 @@
 package com.mrichard.napkin_handler.ui.home;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.mrichard.napkin_handler.databinding.FragmentHomeBinding;
 
+import java.io.IOException;
+
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
 
+    private HomeViewModel homeViewModel;
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        HomeViewModel homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
+        homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
 
+        // Inflating fragment.
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        // We open the gallery.
+        binding.buttonLaunchGallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                galleryActivityResultLauncher.launch(galleryIntent);
+            }
+        });
+
+        // Image showing.
+        homeViewModel.getImageBitmap().observe(getViewLifecycleOwner(), binding.imageViewPicture::setImageBitmap);
+
+        // Image class name binding.
         homeViewModel.getClassifiedText().observe(getViewLifecycleOwner(), binding.textviewClassified::setText);
+
         return root;
     }
 
@@ -33,4 +57,27 @@ public class HomeFragment extends Fragment {
         super.onDestroyView();
         binding = null;
     }
+
+    /**
+     * Starting gallery activity.
+     * We use this abomination as StartActivityForResult is deprecated.
+     * It is horrible as its readability is next to null... but this is the way.
+     */
+    ActivityResultLauncher<Intent> galleryActivityResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                if (result.getResultCode() == Activity.RESULT_OK) {
+                    Intent data = result.getData();
+                    Uri dat = data.getData();
+
+                    try {
+                        Bitmap imageBitmap = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), dat);
+
+                        // Show image through the homeViewModel.
+                        homeViewModel.getImageBitmap().setValue(imageBitmap);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
 }
--- app/src/main/java/com/mrichard/napkin_handler/ui/home/HomeViewModel.java
+++ app/src/main/java/com/mrichard/napkin_handler/ui/home/HomeViewModel.java
@@ -1,6 +1,7 @@
 package com.mrichard.napkin_handler.ui.home;
 
-import androidx.lifecycle.LiveData;
+import android.graphics.Bitmap;
+
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
@@ -8,12 +9,20 @@ public class HomeViewModel extends ViewModel {
 
     private final MutableLiveData<String> classifiedText;
 
+    private final MutableLiveData<Bitmap> imageBitmap;
+
     public HomeViewModel() {
         classifiedText = new MutableLiveData<>();
         classifiedText.setValue("-");
+
+        imageBitmap = new MutableLiveData<>();
     }
 
-    public LiveData<String> getClassifiedText() {
+    public MutableLiveData<String> getClassifiedText() {
         return classifiedText;
+    }
+
+    public MutableLiveData<Bitmap> getImageBitmap() {
+        return imageBitmap;
     }
 }
