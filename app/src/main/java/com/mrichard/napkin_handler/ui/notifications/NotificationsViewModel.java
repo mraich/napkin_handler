98033223170d1a3d36c5e01d1664e5cbcb53163c
Mester Richard <mrichard@citromail.hu>
2022-10-08 18:52:56 +0200

    NapkinHandler:: Egy új Android alkalmazás.

---
+++ app/src/main/java/com/mrichard/napkin_handler/ui/notifications/NotificationsViewModel.java
@@ -0,0 +1,19 @@
+package com.mrichard.napkin_handler.ui.notifications;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class NotificationsViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public NotificationsViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is notifications fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
