9da943ef9ecd72088baa73a82f9cd7bf1cfbaa4f
Mester Richard <mrichard@citromail.hu>
2022-10-09 12:23:02 +0200

    HomeFragment:: A processImageBitmap függvény használata a galériából beolvasott képnek a feldolgozásánál.

--- app/src/main/java/com/mrichard/napkin_handler/ui/home/HomeFragment.java
+++ app/src/main/java/com/mrichard/napkin_handler/ui/home/HomeFragment.java
@@ -80,15 +80,7 @@ public class HomeFragment extends Fragment {
                     try {
                         Bitmap imageBitmap = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), dat);
 
-                        // Creating thumbnail.
-                        int dimension = Math.min(imageBitmap.getWidth(), imageBitmap.getHeight());
-                        Bitmap thumbnailImage = ThumbnailUtils.extractThumbnail(imageBitmap, dimension, dimension);
-
-                        // Show image through the homeViewModel.
-                        homeViewModel.getThumbnailBitmap().setValue(thumbnailImage);
-
-                        String classifiedName = imageRecognizer.recognize(imageBitmap);
-                        homeViewModel.getClassifiedText().setValue(classifiedName);
+                        processImageBitmap(imageBitmap);
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
