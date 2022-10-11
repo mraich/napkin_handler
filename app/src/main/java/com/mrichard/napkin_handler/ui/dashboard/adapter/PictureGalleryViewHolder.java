8c57ee066078954d55dfa92666ef02707feb76dc
Mester Richard <mrichard@citromail.hu>
2022-10-11 18:29:01 +0200

    PictureGalleryViewHolder:: A felesleges import-oknak az eltávolítása.

--- app/src/main/java/com/mrichard/napkin_handler/ui/dashboard/adapter/PictureGalleryViewHolder.java
+++ app/src/main/java/com/mrichard/napkin_handler/ui/dashboard/adapter/PictureGalleryViewHolder.java
@@ -1,11 +1,7 @@
 package com.mrichard.napkin_handler.ui.dashboard.adapter;
 
-import android.view.View;
-import android.widget.ImageView;
-
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.mrichard.napkin_handler.databinding.FragmentDashboardBinding;
 import com.mrichard.napkin_handler.databinding.PictureGalleryItemBinding;
 
 public class PictureGalleryViewHolder extends RecyclerView.ViewHolder {
