0635d21bbde1125b1d418ef74b11bba953c94775
Mester Richard <mrichard@citromail.hu>
2023-02-02 18:20:59 +0100

    ImageRecognizer:: Az ImageRecognizer mostantól IImageRecognizer is.

--- app/src/main/java/com/mrichard/napkin_handler/data/image_recognition/ImageRecognizer.java
+++ app/src/main/java/com/mrichard/napkin_handler/data/image_recognition/ImageRecognizer.java
@@ -22,7 +22,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-public class ImageRecognizer
+public class ImageRecognizer implements IImageRecognizer
 {
 
     private Context context;
