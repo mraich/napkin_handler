b00ed59e1b0f69b299cbc150d4b0f9754f66a2c6
Mester Richard <mrichard@citromail.hu>
2023-02-04 20:38:16 +0100

    NotificationsFragment:: A Category elemek megjelenítésének a kezdetei.

--- app/src/main/java/com/mrichard/napkin_handler/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/mrichard/napkin_handler/ui/notifications/NotificationsFragment.java
@@ -4,15 +4,24 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
+import com.mrichard.napkin_handler.data.db.NapkinDB;
+import com.mrichard.napkin_handler.data.model.category.Category;
 import com.mrichard.napkin_handler.databinding.FragmentNotificationsBinding;
+import com.mrichard.napkin_handler.ui.adapter.CategoryAdapter;
+
+import java.util.List;
 
 public class NotificationsFragment extends Fragment {
+
+    private NapkinDB napkinDB;
+
+    private CategoryAdapter categoryAdapter;
 
     private FragmentNotificationsBinding binding;
 
@@ -21,11 +30,18 @@ public class NotificationsFragment extends Fragment {
         NotificationsViewModel notificationsViewModel =
                 new ViewModelProvider(this).get(NotificationsViewModel.class);
 
+        // Antipattern, but works.
+        napkinDB = NapkinDB.GetInstance(getContext());
+
         binding = FragmentNotificationsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textNotifications;
-        notificationsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        categoryAdapter = new CategoryAdapter(getContext(), getActivity());
+        binding.recyclerViewCategories.setLayoutManager(new LinearLayoutManager(getActivity().getApplicationContext()));
+        binding.recyclerViewCategories.setAdapter(categoryAdapter);
+
+        napkinDB.categoryDao().getAll().observe(getViewLifecycleOwner(), this::onCategoriesChanged);
+
         return root;
     }
 
@@ -34,4 +50,14 @@ public class NotificationsFragment extends Fragment {
         super.onDestroyView();
         binding = null;
     }
+
+    /**
+     * Showing new categories.
+     *
+     * @param newCategories
+     */
+    private void onCategoriesChanged(List<Category> newCategories) {
+        getActivity().runOnUiThread(() -> categoryAdapter.setCategories(newCategories));
+    }
+
 }
--- app/src/main/java/com/mrichard/napkin_handler/ui/notifications/NotificationsViewModel.java
+++ app/src/main/java/com/mrichard/napkin_handler/ui/notifications/NotificationsViewModel.java
@@ -4,16 +4,24 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.mrichard.napkin_handler.data.model.category.Category;
+
+import java.util.List;
+
 public class NotificationsViewModel extends ViewModel {
 
-    private final MutableLiveData<String> mText;
+    private final MutableLiveData<List<Category>> categories;
 
     public NotificationsViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("");
+        categories = new MutableLiveData<>();
     }
 
-    public LiveData<String> getText() {
-        return mText;
+    public void setCategories(List<Category> categories) {
+        this.categories.setValue(categories);
     }
+
+    public LiveData<List<Category>> getCategories() {
+        return categories;
+    }
+
 }
