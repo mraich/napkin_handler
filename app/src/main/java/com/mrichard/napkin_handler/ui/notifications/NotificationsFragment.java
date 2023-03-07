1b5301c3ec302db4f96466cf8440c4bea10ecd35
Mester Richard <mrichard@citromail.hu>
2023-03-07 19:34:21 +0100

    FragmentNotifications:: Új kategória hozzáadása.

--- app/src/main/java/com/mrichard/napkin_handler/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/mrichard/napkin_handler/ui/notifications/NotificationsFragment.java
@@ -4,12 +4,15 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
+import com.mrichard.napkin_handler.R;
 import com.mrichard.napkin_handler.data.db.NapkinDB;
 import com.mrichard.napkin_handler.data.model.category.Category;
 import com.mrichard.napkin_handler.databinding.FragmentNotificationsBinding;
@@ -39,6 +42,28 @@ public class NotificationsFragment extends Fragment {
         categoryAdapter = new CategoryAdapter(getContext(), getActivity());
         binding.recyclerViewCategories.setLayoutManager(new LinearLayoutManager(getActivity().getApplicationContext()));
         binding.recyclerViewCategories.setAdapter(categoryAdapter);
+
+        // Creating new category.
+        binding.buttonNewCategory.setOnClickListener(view -> {
+            AlertDialog.Builder alert = new AlertDialog.Builder(view.getContext());
+
+            alert.setTitle(getResources().getString(R.string.home_new_category));
+
+            final EditText inputEditText = new EditText(view.getContext());
+            alert.setView(inputEditText);
+
+            alert.setPositiveButton(getResources().getString(R.string.ok), (dialog, whichButton) -> {
+                String value = inputEditText.getText().toString();
+                if (!value.equals("")) {
+                    Thread thread = new Thread(() -> napkinDB.categoryDao().insert(new Category(value)));
+                    thread.start();
+                }
+            });
+
+            alert.setNegativeButton(getResources().getString(R.string.cancel), (dialog, whichButton) -> { });
+
+            alert.show();
+        });
 
         napkinDB.categoryDao().getAll().observe(getViewLifecycleOwner(), this::onCategoriesChanged);
 
