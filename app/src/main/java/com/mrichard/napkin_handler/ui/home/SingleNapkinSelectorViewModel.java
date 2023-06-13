ae1ee8c2a7dd44b384e55d833213b575f15f5159
Mester Richard <mrichard@citromail.hu>
2023-06-13 22:34:39 +0200

    SingleNapkinSelectorViewModel.:: A SingleNapkinSelectorViewModel osztálynak a bevezetése.

---
+++ app/src/main/java/com/mrichard/napkin_handler/ui/home/SingleNapkinSelectorViewModel.java
@@ -0,0 +1,24 @@
+package com.mrichard.napkin_handler.ui.home;
+
+import com.mrichard.napkin_handler.data.viewmodel.NapkinSelectorViewModel;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * This is a Napkin Selector ViewModel.
+ * With this ViewModel we can select single napkins at the same time.
+ */
+public class SingleNapkinSelectorViewModel extends NapkinSelectorViewModel {
+
+    public void onClickPicture(Long id) {
+        Set<Long> selectedPicturesSet = new HashSet<>();
+        selectedPicturesSet.add(id);
+
+        selectedPictures.setValue(selectedPicturesSet);
+
+        // Refreshing. :)
+        selectedPictures.setValue(selectedPictures.getValue());
+    }
+
+}
