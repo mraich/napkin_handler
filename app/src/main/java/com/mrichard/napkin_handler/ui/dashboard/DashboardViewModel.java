8211e3104e06f4bdaea243b12a307101d0280aaf
Mester Richard <mrichard@citromail.hu>
2023-06-13 22:30:47 +0200

    DashboardViewModel:: A DashboardViewModel általánosítása NapkinSelectorViewModel-re.

--- app/src/main/java/com/mrichard/napkin_handler/ui/dashboard/DashboardViewModel.java
+++ app/src/main/java/com/mrichard/napkin_handler/ui/dashboard/DashboardViewModel.java
@@ -1,38 +1,12 @@
 package com.mrichard.napkin_handler.ui.dashboard;
 
-import androidx.lifecycle.LiveData;
-import androidx.lifecycle.MutableLiveData;
-import androidx.lifecycle.ViewModel;
+import com.mrichard.napkin_handler.data.viewmodel.NapkinSelectorViewModel;
 
-import com.mrichard.napkin_handler.data.db.NapkinDB;
-import com.mrichard.napkin_handler.data.model.picture.Picture;
-
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
-public class DashboardViewModel extends ViewModel {
-
-    private NapkinDB napkinDB;
-
-    // The ids of the selected pictures.
-    private MutableLiveData<Set<Long>> selectedPictures = new MutableLiveData<>();
-
-    public DashboardViewModel() {
-        selectedPictures.setValue(new HashSet<>());
-    }
-
-    public void setNapkinDB(NapkinDB napkinDB) {
-        this.napkinDB = napkinDB;
-    }
-
-    public LiveData<List<Picture>> getPictures() {
-        return napkinDB.pictureDao().getAll();
-    }
-
-    public LiveData<Set<Long>> selectedPictures() {
-        return selectedPictures;
-    }
+/**
+ * This is a Napkin Selector ViewModel.
+ * With this ViewModel we can select multiple napkins at the same time.
+ */
+public class DashboardViewModel extends NapkinSelectorViewModel {
 
     public void onClickPicture(Long id) {
         if (!selectedPictures.getValue().contains(id)) {
