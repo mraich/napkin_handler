0997861e4185eed8f6d4f31b7ccff51f31e145b6
Mester Richard <mrichard@citromail.hu>
2023-06-13 22:37:17 +0200

    PictureGalleryAdapter:: Néhány felesleges adattagnak, és függvénynek az eltávolítása.

--- app/src/main/java/com/mrichard/napkin_handler/ui/adapter/PictureGalleryAdapter.java
+++ app/src/main/java/com/mrichard/napkin_handler/ui/adapter/PictureGalleryAdapter.java
@@ -10,23 +10,8 @@ import java.util.Set;
 
 public class PictureGalleryAdapter extends PictureGalleryAdapterBase {
 
-    private Set<Long> selectedPictures;
-
     public PictureGalleryAdapter(Context context, Activity activity, NapkinSelectorViewModel napkinSelectorViewModel) {
         super(context, activity, napkinSelectorViewModel);
-    }
-
-    public void setSelectedPictures(Set<Long> selectedPictures) {
-        this.selectedPictures = selectedPictures;
-    }
-
-    // Showing the selection of a picture.
-    private void showSelection(PictureGalleryViewHolder holder, Picture picture) {
-        if (selectedPictures != null && selectedPictures.contains(picture.getId())) {
-            holder.getBinding().getRoot().setBackgroundColor(context.getResources().getColor(com.google.android.material.R.color.material_blue_grey_800));
-        } else {
-            holder.getBinding().getRoot().setBackgroundColor(context.getResources().getColor(com.google.android.material.R.color.m3_ref_palette_white));
-        }
     }
 
 }
